2511b84d9e9c4b9e39aa0e6c4bde8df88b507ee8
Roman <romansvet@gmail.com>
2021-11-04 00:00:55 +0200

    extend logging

--- main.go
+++ main.go
@@ -102,6 +102,7 @@ func Start(SeedConfig Config) {
 
 	logger.Info("tenderseed",
 		"key", nodeKey.ID(),
+		"key path", nodeKeyFilePath,
 		"listen", SeedConfig.ListenAddress,
 		"chain", chainID,
 		"strict-routing", SeedConfig.AddrBookStrict,
